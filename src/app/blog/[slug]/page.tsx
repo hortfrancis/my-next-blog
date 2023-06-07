11c38c65bac2442917536e820282e91861d80d8c
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-06-07 11:39:55 +0100

    Create basic blog structure

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,7 @@
+
+export default function BlogPostPage() {
+    return (
+      <div>Blog post</div>
+    )
+  }
+  
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,6 @@
+
+export default function BlogListPage() {
+  return (
+    <div>BlogListPagepage</div>
+  )
+}
