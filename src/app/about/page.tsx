ce58286ad82a90349de903d3b7725882f6c380ff
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-06-09 15:46:42 +0100

    Fix rogue apostrophe
    Fix apostrophe in /app/about/page.tsx by adding escape character. ESLint would stop build otherwise.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
   return (
     <div>
       <h2>About</h2>
-      <p>Hi, I'm a software engineer...</p>
+      <p>Hi, I&apos;m a software engineer...</p>
     </div>
   )
 }
