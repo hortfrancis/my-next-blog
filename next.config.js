87102b6ef28c71f7a9a2757f67185de513166732
Alex Hort-Francis <alex.hortfrancis@gmail.com>
2023-06-23 16:48:18 +0100

    Tidy up home page
    Change meta description
    
    Add redirect from /posts to /

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-const { withContentlayer } = require('next-contentlayer')
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    
+    async redirects() {
+        return [
+            {
+                source: '/posts',
+                destination: '/',
+                permanent: true,
+            },
+        ]
+    },
+};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import { getPostBySlug, getPosts } from "@/lib/posts";
-
-
-import {compareDesc, format, parseISO } from 'date-fns';
-import {  } from 'contentlayer/generated';
-
-// import { getPosts } from "@/lib/posts";
 import Link from "next/link";
+import { getPosts } from "@/lib/posts";
+import {compareDesc, format, parseISO } from 'date-fns';
 
-// export default async function BlogListPage() {
-//   const posts = await getPosts();
 
-
-// A list of all the blog posts
+/**
+ * Renders a list of posts as a <ul> (unordered list).
+ * Uses the `getPosts()` function, imported from `/lib/posts.ts`.
+ * @returns {JSX.Element} The rendered list of posts.
+ */
 function ListOfPosts() {
   return (
     <div>
@@ -34,64 +30,21 @@ function ListOfPosts() {
 }
 
 
-
 export default function HomePage() {
 
   return (
-    <main>
+      
+    <>
       <h1>My NextJS Blog</h1>
-      <p>Created by <a href="https://github.com/hortfrancis">Alex Hort-Francis</a>, while learning NextJS 13 with <a href="https://techeducators.co.uk">Tech Educators</a>.</p>
 
-    <ListOfPosts />
+      <p>I made this blog while studying NextJS 13, with <a href="https://techeducators.co.uk">Tech Educators</a>, in June 2023.</p>
+      <p>This site is hosted on <a href="https://vercel.com/">Vercel</a>, and I used <a href="https://tailwindcss.com/">Tailwind</a> to style it.</p>
+      <p>Feel free to peruse the blog posts below if you would like to find out more about how I made it, and what I learned! 😊</p>
 
-    </main>
+      <h2>Blog posts</h2>
+      <ListOfPosts />
+
+    </>
+
   );
 }
-
-
-  // function PostCard(post: Post) {
-  //   return (
-  //     <div className="mb-8">
-  //       <h2 className="mb-1 text-xl">
-  //         <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
-  //           {post.title}
-  //         </Link>
-  //       </h2>
-  //       <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-  //         {format(parseISO(post.date), 'LLLL d, yyyy')}
-  //       </time>
-  //       <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
-  //     </div>
-  //   )
-  // }
-  
-  // export default function Home() {
-  //   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
-  
-  //   return (
-  //     <div className="mx-auto max-w-xl py-8">
-  //       <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
-  //       {posts.map((post, idx) => (
-  //         <PostCard key={idx} {...post} />
-  //       ))}
-  //     </div>
-  //   )
-  // }
-
-//   return (
-//     <main className="flex min-h-screen flex-col items-center justify-around p-24">
-//       <h1 className="text-xl">Blog Page</h1>
-//       <ul>
-//         {posts.map((post) => {
-//           return (
-//             <div key={post.slug}>
-//               <li>
-//                 <Link href={`/blog/${post.slug}`}>{post.title}</Link>
-//               </li>
-//             </div>
-//           );
-//         })}
-//       </ul>
-//     </main>
-//   );
-// }
